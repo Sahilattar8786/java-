3c791bf0d3f589e3237e2c2d460a18dfdd51ea10
sahil <sahilattar87860@gmail.com>
2025-09-13 16:03:38 +0530

    Calling constructor from another constructor

--- OOPS/src/ConstructorOverloading.java
+++ OOPS/src/ConstructorOverloading.java
@@ -2,7 +2,9 @@ public class ConstructorOverloading {
     public static void main(String[] args) {
         SmartStudent sahil=new SmartStudent(1,"Sahil");
         SmartStudent second=new SmartStudent(sahil);
-        System.out.println(second.name);
+        SmartStudent third=new SmartStudent();
+        System.out.println(third.name);
+        System.out.println(third.roll_no);
     }
 
 
@@ -13,6 +15,10 @@ class SmartStudent{
      SmartStudent(int roll_no, String name){
          this.roll_no=roll_no;
          this.name=name;
+     }
+     // calling constructor from another constructor
+     SmartStudent (){
+         this(12,"Sahil") ;
      }
      SmartStudent( SmartStudent other){
          this.roll_no=other.roll_no;
